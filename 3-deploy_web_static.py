2105fce2b85008b62ad8faf086f5d2a636e2c3ae
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-05-03 18:55:49 -0700

    another try

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,14 +12,14 @@ env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_pack():
-    """Create tar archive of the directory web_static"""
-    time = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(time.year,
-                                                         time.month,
-                                                         time.day,
-                                                         time.hour,
-                                                         time.minute,
-                                                         time.second)
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
@@ -29,11 +29,12 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to a web server.
-
+    """Distributes an archive to a web server.
     Args:
-        archive_path (str): The path of the archive
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
         return False
@@ -68,7 +69,7 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """Create and distribute an archive to a web server"""
+    """Create and distribute an archive to a web server."""
     file = do_pack()
     if file is None:
         return False
