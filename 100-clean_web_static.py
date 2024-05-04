61b38b947927aca490c7e3eae4e45b8756dd5c23
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-05-03 19:02:11 -0700

    clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Fabric script to delete out-of-date archives."""
+
+from fabric.api import *
+import os
+
+
+env.hosts = ['18.204.7.236', '52.91.128.49']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_clean(number=0):
+    """Delete out-of-date archives."""
+    number = int(number)
+    if number < 1:
+        number = 1
+
+    with lcd("versions"):
+        archives = local("ls -t", capture=True).split()
+
+    for archive in archives[number:]:
+        local("rm -f versions/{}".format(archive))
+
+    with cd("/data/web_static/releases"):
+        releases = run("ls -t").split()
+        for release in releases[number:]:
+            run("rm -rf /data/web_static/releases/{}".format(release))
