1c034f4d44f3f67daf2858643d59c00e83b24573
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-05-19 05:05:54 -0700

    cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with a list of all State objects."""
+    states = storage.all("State")
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
